86291340dcaa487a8faa44682ad31bd40ccd0bf0
Frankie Ziman <FNZiman@gmail.com>
2018-01-18 11:44:45 -0500

    updated json package, webpack, and gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,43 @@
+var path = require('path');
+var webpack = require("webpack");
+
+var plugins = [];
+var devPlugins = [];
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
 module.exports = {
   entry: './lib/main.js',
   output: {
-    filename: 'bundle.js',
+    filename: './lib/bundle.js',
+  },
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
   },
   devtool: 'source-map',
   resolve: {
